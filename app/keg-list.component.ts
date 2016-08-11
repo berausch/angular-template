e1ed7da6f1858ef9dc48dbdb2a755421d8903be4
Brittany Rausch and Shradha Pulla <brittany+shradha@>
2016-08-11 09:18:45 -0700

    Create edit keg functionality

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -1,19 +1,33 @@
 import { Component, EventEmitter } from "angular2/core";
 import { KegComponent } from "./keg.component";
 import { Keg } from "./keg.model";
+import { EditKegComponent } from "./edit-keg.component";
 
 
 @Component ({
   selector: "keg-list",
   inputs: ["kegList"],
-  directives: [KegComponent],
+  outputs: ['onKegSelect'],
+  directives: [KegComponent, EditKegComponent],
   template: `
   <keg-display *ngFor="#currentKeg of kegList"
+  (click)="kegClicked(currentKeg)"
   [keg]="currentKeg" class="col-lg-4">
   </keg-display>
+  <edit-keg *ngIf="selectedKeg" [keg]="selectedKeg">
+  </edit-keg>
   `
 })
 
 export class KegListComponent {
   public kegList: Keg[];
+  public onKegSelect: EventEmitter<Keg>;
+  public selectedKeg: Keg;
+  constructor() {
+    this.onKegSelect = new EventEmitter();
+  }
+  kegClicked(clickedKeg: Keg): void {
+    this.selectedKeg = clickedKeg;
+    this.onKegSelect.emit(clickedKeg);
+  }
 }
